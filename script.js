cac61977f581707c84d88f1ffc430fa503341d54
May0105 <88717349+May0105@users.noreply.github.com>
2025-03-10 14:52:03 +0700

    Fix bug

--- script.js
+++ script.js
@@ -47,14 +47,14 @@ const renderBanner = () => {
   bannerSlides.forEach((slide) => {
     splideListEl.innerHTML += `
         <li key=${slide.id}
-          class="splide__slide relative z-1 flex flex-col justify-end max-w-[var(--container-width)] h-[720px] px-12 pb-[50px] bg-[linear-gradient(180deg,rgba(25,25,25,0)_52.85%,#191919_100%),url('${slide.url}')] bg-cover bg-center">
-          <div class="flex items-end justify-between mb-10">
+          class="splide__slide relative z-1 flex flex-col justify-end max-w-[var(--container-width)] h-[720px] px-4 lg:px-12 pb-[50px] bg-[linear-gradient(180deg,rgba(25,25,25,0)_52.85%,#191919_100%),url('${slide.url}')] bg-cover bg-center">
+          <div class="flex flex-col lg:flex-row items-center lg:items-end justify-between mb-10">
 
             <!-- Left Content -->
-            <div class="text-white">
-              <h1 class="max-w-[429px] mb-6 text-6xl md:text-7xl xl:text-[120px] font-bold uppercase line-height-normal">${slide.title}</h1>
-              <div class="flex items-center gap-3 text-[var(--primary-text-color)]">
-                <span class="text-sm max-w-[347px] h-[54px] leading-[18px]">
+            <div class="text-white mb-8 w-full">
+              <h1 class="max-w-full lg:max-w-[429px] mb-4 lg:mb-6 text-7xl md:text-8xl xl:text-[120px] font-bold uppercase line-height-normal">${slide.title}</h1>
+              <div class="flex items-center justify-between lg:justify-normal gap-0 lg:gap-3 text-[var(--primary-text-color)]">
+                <span class="text-sm max-w-[297px] lg:max-w-[347px] h-[54px] leading-[18px]">
                  ${slide.description}
                 </span>
                 <button
@@ -62,7 +62,7 @@ const renderBanner = () => {
                   <svg
                     width="163"
                     height="49"
-                    class="absolute"
+                    class="absolute w-[158px] lg:w-[163px] h-[40px] lg:h-[49px]"
                     viewBox="0 0 163 49"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg">
@@ -71,14 +71,14 @@ const renderBanner = () => {
                         fill-rule="evenodd"
                         clip-rule="evenodd"
                         d="M0.763961 37.6087C-1.71886 42.9133 2.15269 49 8.00958 49H138.795C141.9 49 144.724 47.2034 146.04 44.3913L161.486 11.3913C163.969 6.0867 160.097 0 154.24 0H23.4552C20.3503 0 17.5258 1.79656 16.2096 4.60869L0.763961 37.6087Z" />
-                    </mask>
+                   </mask>
                     <path
                       d="M16.2096 4.60869L15.3039 4.18478L16.2096 4.60869ZM161.486 11.3913L160.58 10.9674L161.486 11.3913ZM146.04 44.3913L145.135 43.9674L146.04 44.3913ZM0.763961 37.6087L-0.141742 37.1848L0.763961 37.6087ZM8.00958 50H138.795V48H8.00958V50ZM146.946 44.8152L162.392 11.8152L160.58 10.9674L145.135 43.9674L146.946 44.8152ZM154.24 -1H23.4552V1H154.24V-1ZM15.3039 4.18478L-0.141742 37.1848L1.66966 38.0326L17.1153 5.0326L15.3039 4.18478ZM23.4552 -1C19.9622 -1 16.7846 1.02113 15.3039 4.18478L17.1153 5.0326C18.267 2.57199 20.7384 1 23.4552 1V-1ZM162.392 11.8152C165.185 5.84754 160.829 -1 154.24 -1V1C159.365 1 162.753 6.32586 160.58 10.9674L162.392 11.8152ZM138.795 50C142.288 50 145.465 47.9789 146.946 44.8152L145.135 43.9674C143.983 46.428 141.512 48 138.795 48V50ZM8.00958 48C2.8848 48 -0.502802 42.6741 1.66966 38.0326L-0.141742 37.1848C-2.93491 43.1525 1.42058 50 8.00958 50V48Z"
                       fill="#D9D9D9"
                       mask="url(#path-1-inside-1_1_791)" />
                   </svg>
                   <span
-                    class="relative z-10 text-[var(--primary-text-color)] text-base leading-[20px] w-[107px] h-[20px] font-medium text-center whitespace-nowrap overflow-hidden">
+                    class="relative z-10 text-[var(--primary-text-color)]  text-sm lg:text-base leading-[20px] w-[107px] h-[20px] font-medium text-center whitespace-nowrap overflow-hidden">
                     Find out more
                   </span>
                 </button>
@@ -86,18 +86,17 @@ const renderBanner = () => {
             </div>
 
             <!-- Right Content -->
-            <div class="relative max-w-[382px] -mb-10">
+             <div class="relative max-w-full lg:max-w-[382px] -mb-10">
               <!-- Shadow Layer -->
               <div
-                class="absolute w-[100px] h-[100px] left-[110px] top-[-45px] [background:radial-gradient(50%_50%_at_50%_50%,#E30613_0%,#009FE3_100%)] blur-[50px]"></div>
+                class="absolute w-[100px] h-[100px] right-[146px] lg:right-0 lg:left-[110px] top-[-45px] [background:radial-gradient(50%_50%_at_50%_50%,#E30613_0%,#009FE3_100%)] blur-[50px]"></div>
               <div
-                class="absolute w-[168px] h-[168px] left-[163px] top-[0] [background:radial-gradient(50%_50%_at_50%_50%,rgba(0,159,227,0.5)_0%,rgba(227,6,19,0.5)_100%)] blur-[50px]"></div>
-
+                class="absolute w-[168px] h-[168px] bottom-0 lg:bottom-normal right-[48px] lg:right-normal lg:left-[163px] lg:top-[0] [background:radial-gradient(50%_50%_at_50%_50%,rgba(0,159,227,0.5)_0%,rgba(227,6,19,0.5)_100%)] blur-[50px]"></div>
               <!-- Flag item -->
               <img
                 src="./assets/images/flag.png"
                 alt="Racing flag"
-                class="absolute bottom-[98px] left-[-14px] w-[87px] h-[87px] z-1 object-cover" />
+                class="absolute bottom-[62px] lg:bottom-[98px] left-[-14px] w-[87px] h-[87px] z-1 object-cover" />
 
               <div class="content__inner relative">
                 <div
@@ -122,7 +121,7 @@ const renderBanner = () => {
               </div>
 
               <!-- Phone Number -->
-              <div class="absolute top-[131px] right-[5px]">
+              <div class="absolute top-[95px] lg:top-[131px] right-[5px]">
                 <div
                   class="phone-number__inner relative flex gap-[10px] items-center w-[178px] bg-[var(--primary-text-color)] rounded-[24px]">
                   <span
