95e85b0307011be409bbb98c023b3b3ed2714e7d
May0105 <88717349+May0105@users.noreply.github.com>
2025-03-11 08:52:16 +0700

    Updated data image for banner

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bannerSlides = [
   {
     id: 1,
-    url: './assets/images/banner/image-go-kart.png',
+    url: './assets/images/banner/image-go-karts.png',
     alt: 'image-go-karts',
     title: 'go karts',
     description:
